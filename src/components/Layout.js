8f38fe9f5d7b426179978ebbd9496268fbad663c
Nurzhigit <nurcha.mambetov@mail.ru>
2020-09-03 14:21:30 +0600

    верстка

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import Navbar from './Navbar'
+// import Home from './Home'
+import './Layout.css'
+
 
 function Layout(props) {
     return (
         <div>
             <Header />
-            <div className='page-wrapper'>
+            <Navbar/>
+            {/* <Home/> */}
+            <div className='page-wrapper bg-light'>
                 {props.children}
             </div>
             <Footer />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,87 @@
-import React, { useEffect } from 'react'
-import List from '../components/products/List'
-import { Container } from 'reactstrap'
-import { fetchData } from '../redux/actions'
-import { connect } from 'react-redux'
+import React, { useState } from 'react';
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption,
+    Button,
+    Container,
+} from 'reactstrap';
+import frizer from "../assets/frizer.jpg"
+import komp from "../assets/komp.jpg"
+import "./Home.css"
 
 
-
-
-function Home(props) {
-    const {fetchData} = props;
-    useEffect(()=>{
-        fetchData();
-    }, [fetchData]);
-    
-    if(props.err){
-        return <h4 className='text-danger'>
-            {props.err.message}
-        </h4>
+const items = [
+    {
+        src: frizer,
+        comment: <p className="HomeCarouselCommentFirst">SuperFrizer</p>,
+        caption: <Button className="HomeCarouselButtonFirst">Get started</Button>
+    },
+    {
+        src: komp,
+        comment: <p className="HomeCarouselCommentSecond">SuperComputer</p>,
+        caption: <Button className="HomeCarouselButtonSecond">Buy right now</Button>
+    },
+    {
+        src: frizer,
+        comment: <p className="HomeCarouselCommentThird">Fast</p>,
+        caption: <Button className="HomeCarouselButtonThird">View details</Button>
     }
+];
+
+const HomeCarousel = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
+
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+    }
+
+    const slides = items.map((item) => {
+        return (
+            <CarouselItem
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                key={item.src}
+            >
+                <img className="img-car" src={item.src} alt={item.altText} />
+                <CarouselCaption className="mb-1 text-center" captionText={item.caption} captionHeader={item.comment} />
+            </CarouselItem>
+        );
+    });
+
     return (
-        <Container>
-            Home
-            <List data={props.data}/>
-        </Container>
-    )
+        <div className="HomeMainDiv">
+            {/* Carousel */}
+            <Container>
+                <Carousel
+                    activeIndex={activeIndex}
+                    next={next}
+                    previous={previous}
+                >
+                    <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+                    {slides}
+                    <CarouselControl className="car-icon-prev" direction="prev" directionText="Previous" onClickHandler={previous} />
+                    <CarouselControl className="car-icon-next" direction="next" directionText="Next" onClickHandler={next} />
+                </Carousel>
+            </Container>
+        </div>
+    );
 }
 
-const mapStateToProps = (state)=>{
-    const {data, loading, err} = state.ProductReducer;
-    return {data, loading, err};
-}
-
-const mapDispatchToProps = (dispatch)=>{
-    return{
-        fetchData: ()=>dispatch(fetchData())
-    }
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default HomeCarousel;
